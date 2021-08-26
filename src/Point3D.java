fb2e5165038a1fb96382bceb9edb253e2fa80c0a
3P003 <3P003@DESKTOP-57R41K8>
2021-08-26 16:37:26 +0900

    휴스타 프리스쿨

---
+++ src/Point3D.java
@@ -0,0 +1,24 @@
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+
+public class Point3D implements AncestorListener{
+
+	@Override
+	public void ancestorAdded(AncestorEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ancestorRemoved(AncestorEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ancestorMoved(AncestorEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
